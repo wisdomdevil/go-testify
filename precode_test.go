3acadfb3817567792ca129b963a26447e8f08300
wisdomdevil <74432905+wisdomdevil@users.noreply.github.com>
2024-03-24 00:38:59 +0500

    Update precode_test.go

--- precode_test.go
+++ precode_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
@@ -11,42 +11,40 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenAllRight(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(totalCount), nil) // здесь нужно создать запрос к сервису
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	require.NotEmpty(t, responseRecorder.Body)
+	_, err := io.ReadAll(responseRecorder.Body)
+	assert.Nil(t, err)
 }
 
-func TestMainHandlerWhenCityWrong(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?city=spb&count="+strconv.Itoa(totalCount), nil) // здесь нужно создать запрос к сервису
+func TestMainHandlerWhenCityNotFound(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=tula", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong city value", responseRecorder.Body)
+	expected := `wrong city value`
+	assert.Equal(t, responseRecorder.Body.String(), expected)
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(totalCount+1), nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	require.NotEmpty(t, responseRecorder.Body)
-	assert.Equal(t, strings.Join(cafeList["moscow"][:totalCount], ","), responseRecorder.Body)
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+	assert.Equal(t, len(list), totalCount)
 }
